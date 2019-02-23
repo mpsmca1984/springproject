34fbc030f789eb45dcedb271ee02ee3f26f5c2b1
mpsmca1984 <mpsmca1984@gmail.com>
2019-02-24 04:21:47 +0530

    spring project added

---
+++ src/springproject/Jenkintest.java
@@ -0,0 +1,10 @@
+package springproject;
+
+public class Jenkintest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my first genkin roject");
+	}
+
+}
