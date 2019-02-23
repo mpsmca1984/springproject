97eb7e570f46c5ef60502d23ddf4e4be2d5a62cd
mpsmca1984 <mpsmca1984@gmail.com>
2019-02-24 04:43:14 +0530

    modified

--- src/springproject/Jenkintest.java
+++ src/springproject/Jenkintest.java
@@ -4,7 +4,8 @@ public class Jenkintest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is my first genkin roject");
+		System.out.println("This is my first genkin roject");
+		System.out.println("Well done");
 	}
 
 }
